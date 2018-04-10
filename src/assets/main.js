6e71f7be52c3e9f0119548cb32021b965be55755
gwu205 <38195698+gwu205@users.noreply.github.com>
2018-04-10 11:30:39 +1000

    complete

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,10 +1,11 @@
 let answer = document.getElementById('answer');
 let attempt = document.getElementById('attempt');
+let results = document.getElementById('results');
 
 function guess() {
     let input = document.getElementById('user-guess');
     //add functionality to guess function here
-    if (answer == "") {
+    if (answer.value == "") {
       setHiddenFields();
     }
     if (attempt == "") {
@@ -12,10 +13,24 @@ function guess() {
     }
     if (validateInput(input.value) == true) {
       attempt++;
-      console.log('test');
     }
     else {
       return false;
+    }
+
+    var result = getResults(input.value);
+    if (result == true) {
+      setMessage("You Win!!!");
+      showAnswer(result);
+      showReplay();
+    }
+    else if (result == false && attempt > 10) {
+      setMessage("You Lose!");
+      showAnswer(result);
+      showReplay();
+    }
+    else {
+      setMessage("Incorrect, try again.");
     }
 }
 
@@ -44,5 +59,46 @@ function validateInput(userGuess) {
 }
 
 function getResults(input) {
-  //document.getElementById('results').innerHTML =
+  var recordAttempt = "<div class=\"row\"><span class=\"col-md-6\">" +
+  input +
+  "</span><div class=\"col-md-6\">";
+  var correct = 0;
+  for (var i = 0; i < input.length; i++) {
+    if (answer.value.charAt(i) == input.charAt(i)) {
+      recordAttempt += "<span class=\"glyphicon glyphicon-ok\"></span>";
+      correct++;
+    }
+    else if (answer.value.indexOf(input.charAt(i)) != -1) {
+      recordAttempt += "<span class=\"glyphicon glyphicon-transfer\"></span>";
+    }
+    else {
+      recordAttempt += "<span class=\"glyphicon glyphicon-remove\"></span>";
+    }
+  }
+  recordAttempt += "</div></div>"
+
+  results.innerHTML += recordAttempt;
+
+  if (correct == 4) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+function showAnswer(boolean) {
+  var code = document.getElementById('code');
+  if (boolean == true) {
+    code.className += " success";
+  }
+  else {
+    code.className += " failure";
+  }
+  code.innerHTML = answer.value
+}
+
+function showReplay() {
+  document.getElementById('guessing-div').style.display = "none";
+  document.getElementById('replay-div').style.display = "block";
 }
